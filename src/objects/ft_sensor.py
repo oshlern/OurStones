4ebf754b720302cd7caf827ba0a9de5dd2f1af56
Osher Lerner <osherler@gmail.com>
2022-05-06 01:21:42 -0700

    integrated ft sensor subscriber

--- src/objects/ft_sensor.py
+++ src/objects/ft_sensor.py
@@ -1,9 +1,15 @@
 import numpy as np
+import rospy
 
 class ForceTorqueSensor(Object):
     def __init__(self):
-        pass
+        self.wrench = None
+        rospy.init_node('ft_sub')
+        sub = rospy.Subscriber('/ft', Wrench, self.update_data)
+        rospy.spin()
+    
+    def update_data(self, wrench):
+        self.wrench = wrench
 
     def read_force_torque(self): # smoothing?
-        FT = None # TODO
-        return FT
+        return self.wrench
